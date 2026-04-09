409cfb53d518fab87e4fa7447d6629fd99e4ba9a
felipepaloschii <felipe_paloschi@estudante.sesisenai.org.br>
2026-04-09 11:28:09 -0300

    add - arrumei if e coloquei resultados

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById("FormDoacao").addEventListener("submit"), function (e) {
+document.getElementById("FormDoacao").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    let nome = document.getElementById("nome").value.trim();
+    let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
@@ -9,13 +9,31 @@ document.getElementById("FormDoacao").addEventListener("submit"), function (e) {
     let telefone = document.getElementById("telefone").value;
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
+    let resultado = document.getElementById("resultado");
+    let sobrenome = nome.trim().split(/\s+/);
 
-    if (nomeInput.split('').length < 2) {
+    if (sobrenome.length < 2) {
         alert ("Por favor, digite seu nome e sobrenome");
-        return false;
     }
-    if (!email.includes("@gmail.com" || "@outlook.com" || "@hotmail.com")) return alert ("Email Inválido");
-    if (idade < 16) return alert("Deve ser maior de 16 anos");
-    if (peso < 50) return alert("Você deve ter mais de 50kg");
+    if (!email.includes("@")|| !email.includes(".")) {
+        return alert ("Email Inválido! Deve conter @.....com");
+    }
+    if (Number(idade) < 16) return alert("Deve ser maior de 16 anos");
 
-}
+    if (Number(peso) < 50) return alert("Você deve ter mais de 50kg");
+
+    if (isNaN(telefone)){
+        return alert ("O telefone deve conter apenas números");
+    }
+
+    resultado.classList.remove ("popup");
+    resultado.style.display = "block";
+    resultado.innerHTML =
+    "Cadastro realizado com sucesso!" + "<br>" +
+    "Nome: " + nome + "<br>"
+    "Email: " + email + "<br>"
+    "Idade: " + idade + "<br>"
+    "Telefone: " + telefone + "<br>"
+    "Cidade: " + cidade + "<br>"
+    "Estado: " + estado;
+});
