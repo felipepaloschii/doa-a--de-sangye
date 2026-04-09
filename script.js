25adf88badbf0bde8e0e7c14d371c280689ed4f9
felipepaloschii <felipe_paloschi@estudante.sesisenai.org.br>
2026-04-09 08:37:22 -0300

    add - let java

---
+++ script.js
@@ -0,0 +1,13 @@
+function validaca() {
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipo = document.querySelector('input[nome="tipo"]checked');
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+
+
+}
