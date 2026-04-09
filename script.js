c7ed07ec95930ae44d74ba5d481f9c4cc87d870f
felipepaloschii <felipe_paloschi@estudante.sesisenai.org.br>
2026-04-09 09:10:37 -0300

    add - if nome

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function validaca() {
+function validacao(){ 
 
-    let nome = document.getElementById("nome").value;
+document.getElementById("FormDoacao").addEventListener("submit"), function (validacao) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value.trim();
     let email = document.getElementById("email").value;
     let idade = document.getElementById("idade").value;
     let peso = document.getElementById("peso").value;
@@ -9,9 +12,13 @@ function validaca() {
     let cidade = document.getElementById("cidade").value;
     let estado = document.getElementById("estado").value;
 
-    if (!email.includes("0")) return alert ("Email Inválido");
+    if (nomeInput.split('').length < 2) {
+        alert ("Por favor, digite seu nome e sobrenome");
+        return false;
+    }
+    if (!email.includes("@gmail.com" || "@outlook.com" || "@hotmail.com")) return alert ("Email Inválido");
     if (idade < 16) return alert("Deve ser maior de 16 anos");
     if (peso < 50) return alert("Você deve ter mais de 50kg");
 
-    
+}
 }
